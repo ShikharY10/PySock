001df7451a0035ac16890a230ceaa5392524e92a
SHIKHAR YADAV <65106263+ShikharY10@users.noreply.github.com>
2021-08-03 02:17:32 +0530

    added project-urls

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
-DESCRIPTION = 'High level server and client development with E2E encryption.'
+VERSION = '0.0.2'
+DESCRIPTION = 'Pyckage for creating high level multi-client server with E2E encryption.'
 LONG_DESCRIPTION = 'A package that allows to code a server which can handle multiple connections at a time with E2E encryption. It also allows two client to talk together with the help of server'
 
 # Setting up
@@ -20,7 +20,11 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
-    url = "https://github.com/ShikharY10/PySocket",
+    url = "https://github.com/ShikharY10/PySock",
+    project_urls={
+    'Github': 'https://github.com/ShikharY10/PySock',
+    'Shikhar Yadav': 'https://github.com/ShikharY10',
+    },
     packages=find_packages(),
     install_requires=['PyYAML','cryptography'],
     keywords=['socket', 'tcp', 'stream', 'encrypted', 'E2E', 'multi-client-server'],
